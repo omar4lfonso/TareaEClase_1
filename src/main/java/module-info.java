4ff675cd020700f3850350db6dd22c09c7ebf0e8
Omar Solis R <omsolis@estudiantec.cr>
2022-04-02 20:44:36 -0600

    Minor commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,7 @@ module com.example.tareae1_chat_cliente_servidor {
 
     opens com.example.tareae1_chat_cliente_servidor to javafx.fxml;
     exports com.example.tareae1_chat_cliente_servidor;
+
+    opens com.example.tareae1_chat_cliente_servidor.controlador to javafx.fxml;
+    exports com.example.tareae1_chat_cliente_servidor.controlador;
 }
